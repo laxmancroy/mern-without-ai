efb12e4cdfb8072d79425d01735451ac1641da10
Laxman Chandra Roy <laxmanroy.me@gmail.com>
2026-05-01 19:48:02 +0600

    minimal footer modified

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,7 @@
 export function Footer() {
     return (
-        <div>This is footer</div>
+        <footer className="bg-gray-600 text-white text-center py-5 text-xl">
+            <span>Laxman Chandra Roy @ 2026</span>
+        </footer>
     )
 }
