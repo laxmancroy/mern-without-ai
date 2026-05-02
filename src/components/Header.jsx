ac6f0a45a7862ffda61fedf4af1c5d6d6ea71520
Laxman Chandra Roy <laxmanroy.me@gmail.com>
2026-05-02 17:48:20 +0600

    responsive header modified

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { IoMenu } from "react-icons/io5";
 
 
@@ -10,30 +11,34 @@ const navBar = [
 ]
 
 export function Header() {
-
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const mobileMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    }
     return (
-        <header className="bg-blue-600 text-white p-4">
+        <header className="relative bg-blue-600 text-white p-4">
             <div className="flex items-center justify-between">
                 <span className="cursor-pointer">Laxman CR</span>
 
                 {/* Desktop Navigation Bar */}
-                {/* <nav className="flex items-center justify-between *:rounded-md *:mr-4 *:hover:bg-white *:hover:text-black *:px-2 *:hover:cursor-pointer">
+                <nav className="sm:flex hidden items-center justify-between *:rounded-md *:mr-4 *:hover:bg-white *:hover:text-black *:px-2 *:hover:cursor-pointer">
                     {
                         navBar.map((item) => (
-                            <span key={item.name}>{item.name}</span>
+                            <div key={item.name}>{item.name}</div>
                         ))
                     }
-                </nav> */}
+                </nav>
 
                 {/* Mobile Navigation Bar */}
-                <IoMenu />
-                <nav className="bg-red-500 absolute top-5 right-3 flex flex-col items-center justify-between *:rounded-md *:mb-4 *:hover:bg-white *:hover:text-black *:px-2 *:hover:cursor-pointer">
+                <span className="sm:hidden" onClick={mobileMenu}><IoMenu /></span>
+                {isMenuOpen && <nav className="bg-blue-600 absolute top-full right-0 flex flex-col items-center justify-between p-5 *:rounded-md *:hover:bg-white *:hover:text-black *:px-2 *:m-2 *:hover:cursor-pointer">
                     {
                         navBar.map((item) => (
                             <span key={item.name}>{item.name}</span>
                         ))
                     }
                 </nav>
+                }
             </div>
         </header>
     )
