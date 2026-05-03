ec9bc9f60e8aa0353dc512a9f0af05c94e048279
Laxman Chandra Roy <laxmanroy.me@gmail.com>
2026-05-03 16:10:33 +0600

    update previous commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
+import { Skills } from './components/Skills';
 
 function App() {
   return (
     <>
       <Header />
       <div className="bg-red-600">This is App</div>
+      <Skills />
       <Footer />
     </>
   )
