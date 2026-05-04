0d65256ccf8cd8453c56c57c1d626b2dc8e0023a
Laxman Chandra Roy <laxmanroy.me@gmail.com>
2026-05-04 19:03:06 +0600

    skill section minimal update

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,23 @@
+const skillList = [
+    {
+        name: 'html',
+        progress: 90
+    },
+    {
+        name: 'css',
+        progress: 80
+    }
+]
+
 export function Skills() {
     return (
-        <div>This is skill sections</div>
+        <div>
+            {
+                skillList.map((val) => (
+                    <h2 key={val.name}>{val.name}</h2>
+                ))
+            }
+
+        </div >
     )
 }
