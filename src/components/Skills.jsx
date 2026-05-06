22448667f75f92a1bc3c2bb7ac3c96e36e3f5c5f
Laxman Chandra Roy <laxmanroy.me@gmail.com>
2026-05-06 09:47:33 +0600

    skill section icons and some design added

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,25 +1,34 @@
+import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaNodeJs, FaGitAlt, FaDocker } from "react-icons/fa6";
+import { BiLogoTailwindCss, BiLogoTypescript, BiLogoPostgresql } from "react-icons/bi";
+
 const skillList = [
-    { name: "HTML", icon: "" },
-    { name: "CSS", icon: "" },
-    { name: "JavaScript", icon: "" },
-    { name: "ReactJS", icon: "" },
-    { name: "NodeJS", icon: "" },
-    { name: "PostgreSQL", icon: "" },
-    { name: "TailwindCSS", icon: "" },
-    { name: "Git", icon: "" },
-    { name: "Docker", icon: "" },
-    { name: "TypeScript", icon: "" }
+    { name: "HTML", icon: <FaHtml5 /> },
+    { name: "CSS", icon: <FaCss3Alt /> },
+    { name: "JavaScript", icon: <FaJs /> },
+    { name: "ReactJS", icon: <FaReact /> },
+    { name: "NodeJS", icon: <FaNodeJs /> },
+    { name: "PostgreSQL", icon: <BiLogoPostgresql /> },
+    { name: "TailwindCSS", icon: <BiLogoTailwindCss /> },
+    { name: "Git", icon: <FaGitAlt /> },
+    { name: "Docker", icon: <FaDocker /> },
+    { name: "TypeScript", icon: <BiLogoTypescript /> }
 ];
 
 export function Skills() {
     return (
-        <ul>
-            {
-                skillList.map((item) => (
-                    <li key={item.name}>{item.name}</li>
-                ))
-            }
+        <>
+            <h2 className="text-center text-4xl">Skills</h2>
+            <ul className="flex justify-center ">
+                {
+                    skillList.map((item) => (
+                        <li key={item.name} className="bg-blue-400 text-white px-3 py-2 m-2">
+                            <span>{item.icon}</span>
+                            <span>{item.name}</span>
+                        </li>
+                    ))
+                }
 
-        </ul>
+            </ul>
+        </>
     )
 }
