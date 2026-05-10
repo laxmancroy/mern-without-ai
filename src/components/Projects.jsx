d2f6104822413ca16a599a2ddb22fe053c82e2bb
Laxman Chandra Roy <laxmanroy.me@gmail.com>
2026-05-10 22:39:18 +0600

    some upgrade in projects sections

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,20 +1,40 @@
 const projectLists = [
-    { name: 'Tech Solutions', description: 'A comprehensive portfolio website for service-based companies developed using Next.js. Utilizes libraries such as React-Awesome-Reveal, react-icons, react-count-up, react-syntax-highlighter, and react-typed.' },
-    { name: 'Tech Solution', description: 'A comprehensive portfolio website for service-based companies developed using Next.js. Utilizes libraries such as React-Awesome-Reveal, react-icons, react-count-up, react-syntax-highlighter, and react-typed.' },
-    { name: 'Tech Solutios', description: 'A comprehensive portfolio website for service-based companies developed using Next.js. Utilizes libraries such as React-Awesome-Reveal, react-icons, react-count-up, react-syntax-highlighter, and react-typed.' },
+    {
+        name: 'Calculator',
+        description: 'A comprehensive portfolio website for service-based companies developed using Next.js. Utilizes libraries such as React-Awesome-Reveal, react-icons, react-count-up, react-syntax-highlighter, and react-typed.',
+        thumb: '/projects-thumb/calculator-thumb.png'
+    },
+    {
+        name: 'Chatbot',
+        description: 'A comprehensive portfolio website for service-based companies developed using Next.js. Utilizes libraries such as React-Awesome-Reveal, react-icons, react-count-up, react-syntax-highlighter, and react-typed.',
+        thumb: '/projects-thumb/chatbot-thumb.png'
+    },
+    {
+        name: 'Random Color Picker',
+        description: 'A comprehensive portfolio website for service-based companies developed using Next.js. Utilizes libraries such as React-Awesome-Reveal, react-icons, react-count-up, react-syntax-highlighter, and react-typed.',
+        thumb: '/projects-thumb/random-color-picker-thumb.png'
+    },
+    {
+        name: 'Random Password Generator',
+        description: 'A comprehensive portfolio website for service-based companies developed using Next.js. Utilizes libraries such as React-Awesome-Reveal, react-icons, react-count-up, react-syntax-highlighter, and react-typed.',
+        thumb: '/projects-thumb/random-password-generator-thumb.png'
+    }
 ];
 export function Projects() {
     return (
         <>
             <h2 className="text-center text-2xl">Projects</h2>
-            {
-                projectLists.map((item) => (
-                    <div key={item.name}>
-                        <h3>{item.name}</h3>
-                        <span>{item.description}</span>
-                    </div>
-                ))
-            }
+            <div className="flex">
+                {
+                    projectLists.map((item) => (
+                        <div key={item.name}>
+                            <img src={item.thumb} alt="" />
+                            <h3>{item.name}</h3>
+                            <span>{item.description}</span>
+                        </div>
+                    ))
+                }
+            </div>
         </>
     )
 }
